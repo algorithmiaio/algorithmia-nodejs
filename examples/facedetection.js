3182ae8a170ff38ecd13e0714034311a878e15ae
Anthony Nowell <anthony@algorithmia.com>
2015-08-04 17:37:31 -0600

    Let facedetecion example run regardless of CWD

--- examples/facedetection.js
+++ examples/facedetection.js
@@ -1,6 +1,6 @@
 /*
 	facedetection.js
-	
+
 	Example shows how to call Algorithmia to process a base64 image. This script will:
 		- Encode a local image to base64
 		- Run the base64 representation through FaceDetection algorithm
@@ -12,7 +12,7 @@ var algorithmia = require("../lib/algorithmia.js"),
 
 var client = algorithmia(process.env.ALGORITHMIA_API_KEY);
 
-var image = "theoffice.jpg";
+var image = __dirname+"/theoffice.jpg";
 var buffer = fs.readFileSync(image).toString("base64");
 
 client.algo("ANaimi/FaceDetection").pipe(buffer).then(function(output) {
