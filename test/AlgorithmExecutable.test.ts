9102b23bcfb53db37e488d12cd1c5bf15a86b778
Dale Herring <dale.herring@ruralsourcing.com>
2020-12-22 17:54:44 -0600

    DEV86 - add done to all tests

--- test/AlgorithmExecutable.test.ts
+++ test/AlgorithmExecutable.test.ts
@@ -7,10 +7,11 @@ describe("Localisation initialization", () => {
     });
 
     describe('algorithm pipe call', () => {
-        it('invokes algorithm', async() => {
+        it('invokes algorithm', async(done) => {
             let input =  {"user_file" : "data://dherring/DalesFunTime/SpongebobMockingApp.jpg"};
             let expected = 'Before bytes -  14004 after bytes -  24776 result file path - {"imageSaveLocation":"data://dherring/DalesFunTime/save_location.png","rectsSaveLocation":"data://dherring/DalesFunTime/save_location.pngrects.txt"}';
             Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).algo("dherring/ResultFile").pipe(input, '0.5.37').then(x => { assert.equal(x, expected) });
+            done();
         });
     });
 
--- test/Data.test.ts
+++ test/Data.test.ts
@@ -8,7 +8,7 @@ describe("Localisation initialization", () => {
     });
 
     describe('algorithm file put call', () => {
-        it('puts file', async () => {
+        it('puts file', async (done) => {
             let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
 
             await file.exists().then(x => {
@@ -30,49 +30,55 @@ describe("Localisation initialization", () => {
             })
             await file.put('nah dawg');
             expect(file.exists());
+            done();
         });
     });
 
     describe('algorithm file parent', () => {
-        it('gets parent', async() => {
+        it('gets parent', async(done) => {
             let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
             assert(await file.parent()?.baseName() == 'DalesNotSoFunTime2');
+            done();
         });
     });
     
     describe('algorithm file get call', () => {
-        it('gets for file', async() => {
+        it('gets for file', async(done) => {
             let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
             await file.get().then(x => { assert.equal(JSON.parse(x), 'nah dawg' )});
+            done();
         });
     });
 
     
     describe('algorithm file head call', () => {
-        it('checks for file', async() => {
+        it('checks for file', async(done) => {
             let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
             await file.exists().then(x => { assert.equal(x, true) });
+            done();
         });
     });
     
     describe('algorithm file delete call', () => {
-        it('deletes for file', async() => {
+        it('deletes for file', async(done) => {
             let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg2.txt');
             await file.put('nah dawg 2');
             let expected = {"result":{"deleted":1}};
             await file.delete().then(x => { assert.equal(x, JSON.stringify(expected)) });
+            done();
         });
     });
 
     describe('algorithm directory head call', () => {
-        it('checks for directory', async() => {
+        it('checks for directory', async(done) => {
             let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime2');
             await dir.exists().then(x => { assert.equal(x, true) });
+            done();
         });
     });
     
     describe('algorithm directory post call', () => {
-        it('creates dir', async() => {
+        it('creates dir', async(done) => {
             let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime3');
 
             await dir.exists().then(x => {
@@ -83,11 +89,12 @@ describe("Localisation initialization", () => {
          
             await dir.post('DalesNotSoFunTime3');
             expect(dir.exists().then(x => { return x }));
+            done();
         });
     });
     
     describe('algorithm directory file put call', () => {
-        it('puts file', async() => {
+        it('puts file', async(done) => {
             let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime2');
             let file: DataFile = dir.file('YeahDawg.txt');
     
@@ -99,23 +106,26 @@ describe("Localisation initialization", () => {
     
             await dir.put(file.baseName(), 'yeah dawg');
             expect(file.exists().then(x => { return x }));
+            done();
         });
     });
 
     describe('algorithm directory get call', () => {
-        it('gets dir', async() => {
+        it('gets dir', async(done) => {
             let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesFunTime');
             await dir.get().then(x => { let dataList: DataList = JSON.parse(x); assert(dataList.files.length == 6) });
+            done();
         });
     });
     
     describe('algorithm directory delete call', () => {
-        it('deletes directory', async() => {
+        it('deletes directory', async(done) => {
             let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring');
             await dir.post('DalesNotSoFunTime4');
             let newDir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime4');
             let expected = {"result":{"deleted":1}};
             await newDir.delete(true).then(x => { assert.equal(x, JSON.stringify(expected)) });
+            done();
         });
     });
 
