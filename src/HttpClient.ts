e7998a41d8b22f01f5419676bc5ad52323b49012
Nathan Lyle Black <me@lyleblack.com>
2020-12-25 09:30:05 -0800

    lint --fix

--- src/HttpClient.ts
+++ src/HttpClient.ts
@@ -2,41 +2,56 @@ import { HttpClient as TypedHttpClient } from 'typed-rest-client/HttpClient';
 import { IHeaders } from 'typed-rest-client/Interfaces';
 
 class HttpClient {
+  private key: string;
+  private userAgent =
+    '{User-Agent : algorithmia-nodejs/' +
+    process.env.npm_package_version +
+    ' (NodeJS ' +
+    process.version +
+    ')}';
+  private headers: IHeaders = {};
+  private httpClient: TypedHttpClient;
 
-    private key: string;
-    private userAgent = '{User-Agent : algorithmia-nodejs/' + process.env.npm_package_version + ' (NodeJS ' + process.version + ')}';
-    private headers: IHeaders = {};
-    private httpClient: TypedHttpClient;
+  public constructor(key: string) {
+    this.key = key;
+    this.headers['Authorization'] = this.key;
+    this.httpClient = new TypedHttpClient(this.userAgent);
+  }
 
-    public constructor(key: string) {
-        this.key = key;
-        this.headers['Authorization'] = this.key;
-        this.httpClient = new TypedHttpClient(this.userAgent);
-    }
+  async get(path: string) {
+    return await this.httpClient.get(path, this.headers).then((x) => {
+      return x.readBody();
+    });
+  }
 
-    async get(path: string) {
-        return await this.httpClient.get(path, this.headers).then(x => { return x.readBody() });
-    }
+  async head(path: string) {
+    return await this.httpClient.head(path, this.headers).then((x) => {
+      return x.message.statusCode === 200;
+    });
+  }
 
-    async head(path: string) {
-        return await this.httpClient.head(path, this.headers).then(x => {
-            return x.message.statusCode === 200;
-        });
-    }
+  async post(path: string, data: Object, contentType: string) {
+    this.headers['Content-Type'] = contentType;
+    return await this.httpClient
+      .post(path, JSON.stringify(data), this.headers)
+      .then((x) => {
+        return x.readBody();
+      });
+  }
 
-    async post(path: string, data: Object, contentType: string) {
-        this.headers['Content-Type'] = contentType;
-        return await this.httpClient.post(path, JSON.stringify(data), this.headers).then(x => { return x.readBody() });
-    }
+  async put(path: string, data: Object) {
+    return await this.httpClient
+      .put(path, JSON.stringify(data), this.headers)
+      .then((x) => {
+        return x.readBody();
+      });
+  }
 
-    async put(path: string, data: Object) {
-        return await this.httpClient.put(path, JSON.stringify(data), this.headers).then(x => { return x.readBody() });
-    }
-
-    async delete(path: string) {
-        return await this.httpClient.del(path, this.headers).then(x => { return x.readBody() });
-    }
-
+  async delete(path: string) {
+    return await this.httpClient.del(path, this.headers).then((x) => {
+      return x.readBody();
+    });
+  }
 }
 
 export { HttpClient };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ export { Algorithmia } from '../src/Algorithmia';
 export { AlgorithmiaClient } from './AlgorithmiaClient';
 export { DataFile, DataDir, DataList } from '../src/Data';
 import { DataFile } from '../src/Data';
-import {Algorithmia } from '../src/Algorithmia';
+import { Algorithmia } from '../src/Algorithmia';
 
-let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesFunTime/NahDawg.txt');
-            file.get().then(x => {console.log(x)});
+const file: DataFile = Algorithmia.getClient(
+  process.env.ALGORITHMIA_DEFAULT_API_KEY
+).file('data://dherring/DalesFunTime/NahDawg.txt');
+file.get().then((x) => {
+  console.log(x);
+});
