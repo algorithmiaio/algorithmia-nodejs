d78e40adeaa42b4d1376e6e180b458c47ada73d8
Dale Herring <dale.herring@ruralsourcing.com>
2020-12-31 11:56:27 -0600

    DEV86 - remove test data from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 export { Algorithmia } from '../src/Algorithmia';
 export { AlgorithmiaClient } from './AlgorithmiaClient';
 export { DataFile, DataDir, DataList } from '../src/Data';
-import { DataFile } from '../src/Data';
-import {Algorithmia } from '../src/Algorithmia';
-
-let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesFunTime/NahDawg.txt');
-            file.get().then(x => {console.log(x)});
