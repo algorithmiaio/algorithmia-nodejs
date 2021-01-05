5d0658aff3cbb424598cbf04dd6a4bb9ce284857
Nathan Lyle Black <nblack@algorithmia.io>
2021-01-04 16:30:17 -0800

    lint --fix

--- src/Data.ts
+++ src/Data.ts
@@ -2,93 +2,86 @@ import { HttpClient } from './HttpClient';
 import { ContentTypeHelper } from './ContentTypeHelper';
 
 abstract class Data {
+  protected client: HttpClient;
+  protected path: string;
 
-    protected client: HttpClient;
-    protected path: string;
+  public constructor(client: HttpClient, path: string) {
+    this.client = client;
+    this.path = path.replace(/data\:\/\//, '');
+  }
 
-    public constructor(client: HttpClient, path: string) {
-        this.client = client;
-        this.path = path.replace(/data\:\/\//, '');
+  baseName(): string {
+    return this.path.slice(this.path.lastIndexOf('/') + 1);
+  }
+
+  parent(): DataDir | null {
+    const offset = this.path.lastIndexOf('/');
+    if (offset >= 0) {
+      return new DataDir(this.client, `data://${this.path.slice(0, offset)}`);
+    } else {
+      return null;
     }
-
-    baseName(): string {
-        return this.path.slice(this.path.lastIndexOf('/') + 1);
-    }
-
-    parent(): DataDir | null {
-        const offset = this.path.lastIndexOf('/');
-        if (offset >= 0) {
-            return new DataDir(this.client, `data://${this.path.slice(0, offset)}`);
-        }
-        else {
-            return null;
-        }
-    };
-
+  }
 }
 
 /*
-* File objects in the Algorithmia Data API
-*/
+ * File objects in the Algorithmia Data API
+ */
 class DataFile extends Data {
+  get() {
+    return this.client.get(this.path);
+  }
 
-    get() {
-        return this.client.get(this.path);
-    }
+  put(input: string) {
+    return this.client.put(this.path, input);
+  }
 
-    put(input: string) {
-        return this.client.put(this.path, input);
-    }
+  delete() {
+    return this.client.delete(this.path);
+  }
 
-    delete() {
-        return this.client.delete(this.path);
-    }
-
-    exists() {
-        return this.client.head(this.path);
-    }
-
+  exists() {
+    return this.client.head(this.path);
+  }
 }
 
 /*
 # Dir objects in the Algorithmia Data API
 */
 class DataDir extends Data {
+  file(fileName: string) {
+    return new DataFile(this.client, `data://${this.path}/${fileName}`);
+  }
 
-    file(fileName: string) {
-        return new DataFile(this.client, `data://${this.path}/${fileName}`);
-    }
+  get() {
+    return this.client.get(this.path);
+  }
 
-    get() {
-        return this.client.get(this.path);
-    }
+  put(fileName: string, input: string) {
+    return this.file(fileName).put(input);
+  }
 
-    put(fileName: string, input: string) {
-        return this.file(fileName).put(input);
-    }
+  post(input: string) {
+    const contentTypeHelper: ContentTypeHelper = new ContentTypeHelper();
+    let contentType: string;
 
-    post(input: string) {
-        let contentTypeHelper: ContentTypeHelper = new ContentTypeHelper;
-        let contentType: string;
+    contentType = contentTypeHelper.contentTypeHelper(input);
 
-        contentType = contentTypeHelper.contentTypeHelper(input);
+    return this.client.post(this.path, input, contentType);
+  }
 
-        return this.client.post(this.path, input, contentType);
-    }
+  exists() {
+    return this.client.head(this.path);
+  }
 
-    exists() {
-        return this.client.head(this.path);
-    }
-
-    delete(force: Boolean) {
-        return this.client.delete(`${this.path}?force=${force}`)
-    }
-
+  delete(force: boolean) {
+    return this.client.delete(`${this.path}?force=${force}`);
+  }
 }
 
 interface DataList {
-    folders: String[];
-    files: String[];
+  folders: string[];
+  files: string[];
 }
 
 export { DataFile, DataDir, DataList };
--- test/Data.test.ts
+++ test/Data.test.ts
@@ -2,131 +2,177 @@ import { AssertionError, strict as assert } from 'assert';
 import { Algorithmia } from '../src/Algorithmia';
 import { DataFile, DataDir, DataList } from '../src/Data';
 
-describe("Localisation initialization", () => {
-    beforeEach(() => {
-        jest.resetModules();
-    });
+describe('Localisation initialization', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
 
-    describe('algorithm file put call', () => {
-        it('puts file', async (done) => {
-            let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
+  describe('algorithm file put call', () => {
+    it('puts file', async (done) => {
+      const file: DataFile = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
 
-            await file.exists().then(x => {
-                if (x == true) {
-                    file.delete();
-                }       
-            })
-    
-            await file.parent()?.exists().then(x => {
-                if (x == false) {
-                    let dir: DataDir = file.parent()!;
-                    let dirName = dir.baseName();
-                    let dirParentDir = dir.parent();
-                    let dirParentName = dirParentDir?.baseName();
+      await file.exists().then((x) => {
+        if (x == true) {
+          file.delete();
+        }
+      });
 
-                    let dir2: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir(dirParentName!);
-                    dir2.post(dirName);
-                }
-            })
-            await file.put('nah dawg');
-            expect(file.exists());
-            done();
-        });
-    });
+      await file
+        .parent()
+        ?.exists()
+        .then((x) => {
+          if (x == false) {
+            const dir: DataDir = file.parent()!;
+            const dirName = dir.baseName();
+            const dirParentDir = dir.parent();
+            const dirParentName = dirParentDir?.baseName();
 
-    describe('algorithm file parent', () => {
-        it('gets parent', async(done) => {
-            let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
-            assert(await file.parent()?.baseName() == 'DalesNotSoFunTime2');
-            done();
+            const dir2: DataDir = Algorithmia.getClient(
+              process.env.ALGORITHMIA_DEFAULT_API_KEY
+            ).dir(dirParentName!);
+            dir2.post(dirName);
+          }
         });
+      await file.put('nah dawg');
+      expect(file.exists());
+      done();
     });
-    
-    describe('algorithm file get call', () => {
-        it('gets for file', async(done) => {
-            let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesFunTime/NahDawg.txt');
-            await file.get().then(x => { assert.equal(x, 'nah dawg' )});
-            done();
-        });
-    });
+  });
 
-    
-    describe('algorithm file head call', () => {
-        it('checks for file', async(done) => {
-            let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesFunTime/NahDawg.txt');
-            await file.exists().then(x => { assert.equal(x, true) });
-            done();
-        });
+  describe('algorithm file parent', () => {
+    it('gets parent', async (done) => {
+      const file: DataFile = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).file('data://dherring/DalesNotSoFunTime2/NahDawg.txt');
+      assert((await file.parent()?.baseName()) == 'DalesNotSoFunTime2');
+      done();
     });
-    
-    describe('algorithm file delete call', () => {
-        it('deletes for file', async(done) => {
-            let file: DataFile = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).file('data://dherring/DalesNotSoFunTime2/NahDawg2.txt');
-            await file.put('nah dawg 2');
-            let expected = {"result":{"deleted":1}};
-            await file.delete().then(x => { assert.equal(x, JSON.stringify(expected)) });
-            done();
-        });
-    });
+  });
 
-    describe('algorithm directory head call', () => {
-        it('checks for directory', async(done) => {
-            let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime2');
-            await dir.exists().then(x => { assert.equal(x, true) });
-            done();
-        });
+  describe('algorithm file get call', () => {
+    it('gets for file', async (done) => {
+      const file: DataFile = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).file('data://dherring/DalesFunTime/NahDawg.txt');
+      await file.get().then((x) => {
+        assert.equal(x, 'nah dawg');
+      });
+      done();
     });
-    
-    describe('algorithm directory post call', () => {
-        it('creates dir', async(done) => {
-            let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime3');
+  });
 
-            await dir.exists().then(x => {
-                if (x == true) {
-                    dir.delete(true);
-                }       
-            })
-         
-            await dir.post('DalesNotSoFunTime3');
-            expect(dir.exists().then(x => { return x }));
-            done();
-        });
+  describe('algorithm file head call', () => {
+    it('checks for file', async (done) => {
+      const file: DataFile = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).file('data://dherring/DalesFunTime/NahDawg.txt');
+      await file.exists().then((x) => {
+        assert.equal(x, true);
+      });
+      done();
     });
-    
-    describe('algorithm directory file put call', () => {
-        it('puts file', async(done) => {
-            let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime2');
-            let file: DataFile = dir.file('YeahDawg.txt');
-    
-            await file.exists().then(x => {
-                if (x == true) {
-                    file.delete();
-                }
-            })
-    
-            await dir.put(file.baseName(), 'yeah dawg');
-            expect(file.exists().then(x => { return x }));
-            done();
-        });
-    });
+  });
 
-    describe('algorithm directory get call', () => {
-        it('gets dir', async(done) => {
-            let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesFunTime');
-            await dir.get().then(x => { let dataList: DataList = JSON.parse(x); assert(dataList.files.length == 7) });
-            done();
-        });
+  describe('algorithm file delete call', () => {
+    it('deletes for file', async (done) => {
+      const file: DataFile = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).file('data://dherring/DalesNotSoFunTime2/NahDawg2.txt');
+      await file.put('nah dawg 2');
+      const expected = { result: { deleted: 1 } };
+      await file.delete().then((x) => {
+        assert.equal(x, JSON.stringify(expected));
+      });
+      done();
     });
-    
-    describe('algorithm directory delete call', () => {
-        it('deletes directory', async(done) => {
-            let dir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring');
-            await dir.post('DalesNotSoFunTime4');
-            let newDir: DataDir = Algorithmia.getClient(process.env.ALGORITHMIA_DEFAULT_API_KEY).dir('data://dherring/DalesNotSoFunTime4');
-            let expected = {"result":{"deleted":1}};
-            await newDir.delete(true).then(x => { assert.equal(x, JSON.stringify(expected)) });
-            done();
-        });
-    });
+  });
 
+  describe('algorithm directory head call', () => {
+    it('checks for directory', async (done) => {
+      const dir: DataDir = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).dir('data://dherring/DalesNotSoFunTime2');
+      await dir.exists().then((x) => {
+        assert.equal(x, true);
+      });
+      done();
+    });
+  });
+
+  describe('algorithm directory post call', () => {
+    it('creates dir', async (done) => {
+      const dir: DataDir = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).dir('data://dherring/DalesNotSoFunTime3');
+
+      await dir.exists().then((x) => {
+        if (x == true) {
+          dir.delete(true);
+        }
+      });
+
+      await dir.post('DalesNotSoFunTime3');
+      expect(
+        dir.exists().then((x) => {
+          return x;
+        })
+      );
+      done();
+    });
+  });
+
+  describe('algorithm directory file put call', () => {
+    it('puts file', async (done) => {
+      const dir: DataDir = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).dir('data://dherring/DalesNotSoFunTime2');
+      const file: DataFile = dir.file('YeahDawg.txt');
+
+      await file.exists().then((x) => {
+        if (x == true) {
+          file.delete();
+        }
+      });
+
+      await dir.put(file.baseName(), 'yeah dawg');
+      expect(
+        file.exists().then((x) => {
+          return x;
+        })
+      );
+      done();
+    });
+  });
+
+  describe('algorithm directory get call', () => {
+    it('gets dir', async (done) => {
+      const dir: DataDir = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).dir('data://dherring/DalesFunTime');
+      await dir.get().then((x) => {
+        const dataList: DataList = JSON.parse(x);
+        assert(dataList.files.length == 7);
+      });
+      done();
+    });
+  });
+
+  describe('algorithm directory delete call', () => {
+    it('deletes directory', async (done) => {
+      const dir: DataDir = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).dir('data://dherring');
+      await dir.post('DalesNotSoFunTime4');
+      const newDir: DataDir = Algorithmia.getClient(
+        process.env.ALGORITHMIA_DEFAULT_API_KEY
+      ).dir('data://dherring/DalesNotSoFunTime4');
+      const expected = { result: { deleted: 1 } };
+      await newDir.delete(true).then((x) => {
+        assert.equal(x, JSON.stringify(expected));
+      });
+      done();
+    });
+  });
 });
