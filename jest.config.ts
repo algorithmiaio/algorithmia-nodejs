93345c38bdfdbd631ab425756470e2cc01184513
Nathan Lyle Black <nblack@algorithmia.io>
2021-01-05 09:07:38 -0800

    Use ES6 export instead of CommonJS

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,9 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts'],
   verbose: true,
 };
+
+export default config;
