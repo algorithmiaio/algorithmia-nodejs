6eaee520b7d826c59a36731fe8f7eb2052b46677
Dale Herring <dale.herring@ruralsourcing.com>
2021-01-08 15:15:53 -0600

    DEV86 - switch pipe test

--- test/AlgorithmExecutable.test.ts
+++ test/AlgorithmExecutable.test.ts
@@ -8,21 +8,12 @@ describe('Localisation initialization', () => {
 
   describe('algorithm pipe call', () => {
     it('invokes algorithm', async () => {
-      const expected =
-        'Before bytes -  14004 after bytes -  24776 result file path - {"imageSaveLocation":"data://dherring/DalesFunTime/save_location.png","rectsSaveLocation":"data://dherring/DalesFunTime/save_location.pngrects.txt"}';
-
       const result = await Algorithmia.getClient(
         process.env.ALGORITHMIA_DEFAULT_API_KEY
       )
-        .algo('dherring/ResultFile')
-        .pipe(
-          {
-            user_file: 'data://dherring/DalesFunTime/SpongebobMockingApp.jpg',
-          },
-          '0.5.37'
-        );
-
-      expect(result).toBe(expected);
+        .algo('nlb/Hello/1.0.0')
+        .pipe('foo');
+      expect(result).toBe('Hello "foo"');
     });
   });
 });
